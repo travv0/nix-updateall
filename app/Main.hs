96ee6f533e4929a818d1b2d53642e8cb17c170aa
Travis <tssund93@gmail.com>
2020-12-31 00:43:39 -0600

    add verbose flag and refactor

--- app/Main.hs
+++ app/Main.hs
@@ -2,23 +2,26 @@
 
 module Main where
 
+import Data.Text (Text)
 import qualified Data.Text as T
 import Shelly
+import System.Environment (getArgs)
 
 main :: IO ()
-main = shelly $ do
+main = do
+    args <- getArgs
+    shelly $ print_commands ("-v" `elem` args) updateAll
+
+updateAll :: Sh ()
+updateAll = do
     Just home <- get_env "HOME"
     cd $ home </> ("nixos" :: FilePath)
 
-    mgit <- which "git"
-    case mgit of
-        Nothing -> return ()
-        Just git -> run_ git ["pull"]
+    withGitMaybe $ \git -> run_ git ["pull"]
 
-    run_
-        "sudo"
-        [ "nix-channel"
-        , "--add"
+    runSudo_
+        "nix-channel"
+        [ "--add"
         , "https://nixos.org/channels/nixos-unstable"
         , "nixos"
         ]
@@ -29,37 +32,45 @@ main = shelly $ do
         , "home-manager"
         ]
 
-    run_ "sudo" ["nix-channel", "--update"]
+    runSudo_ "nix-channel" ["--update"]
     run_ "nix-channel" ["--update"]
 
-    run_ "sudo" ["nix-collect-garbage", "--delete-older-than", "30d"]
-    run_ "sudo" ["nixos-rebuild", "switch", "--upgrade"]
+    runSudo_ "nix-collect-garbage" ["--delete-older-than", "30d"]
+    runSudo_ "nixos-rebuild" ["switch", "--upgrade"]
     run_ "home-manager" ["switch"]
 
+    withGitMaybe $ \git -> do
+        run_ git ["add", "-A"]
+        setenv "LANG" "C.UTF-8"
+        commitMsg <-
+            run "git" ["-c", "color.status=false", "status"]
+                -|- run
+                    "sed"
+                    [ "-n"
+                    , "-r"
+                    , "-e"
+                    , "1,/Changes to be committed:/ d"
+                    , "-e"
+                    , "1,1 d"
+                    , "-e"
+                    , "/^Untracked files:/,$ d"
+                    , "-e"
+                    , "s/^\\s*//"
+                    , "-e"
+                    , "/^\\(/ d"
+                    , "-e"
+                    , "/./p"
+                    ]
+        unless (T.null commitMsg) $ do
+            run_ git ["commit", "-m", commitMsg]
+            run_ git ["push"]
+
+withGitMaybe :: (FilePath -> Sh ()) -> Sh ()
+withGitMaybe f = do
+    mgit <- which "git"
     case mgit of
+        Just git -> f git
         Nothing -> return ()
-        Just git -> do
-            run_ git ["add", "-A"]
-            setenv "LANG" "C.UTF-8"
-            commitMsg <-
-                run "git" ["-c", "color.status=false", "status"]
-                    -|- run
-                        "sed"
-                        [ "-n"
-                        , "-r"
-                        , "-e"
-                        , "1,/Changes to be committed:/ d"
-                        , "-e"
-                        , "1,1 d"
-                        , "-e"
-                        , "/^Untracked files:/,$ d"
-                        , "-e"
-                        , "s/^\\s*//"
-                        , "-e"
-                        , "/^\\(/ d"
-                        , "-e"
-                        , "/./p"
-                        ]
-            unless (T.null commitMsg) $ do
-                run_ git ["commit", "-m", commitMsg]
-                run_ git ["push"]
+
+runSudo_ :: FilePath -> [Text] -> Sh ()
+runSudo_ fp args = run_ "sudo" (toTextIgnore fp : args)
